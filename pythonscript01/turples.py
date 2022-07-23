1f2b7dd0e6398dd388680a315843ed59907129cf
Braintech22 <datveryjimmy@gmail.com>
2022-07-23 17:14:19 -0400

    heo

--- pythonscript01/turples.py
+++
@@ -1,9 +0,0 @@
-# They can be created by simply declaring a tuple within parentheses, (), or by converting any sequence into a tuple using the
-# built-in constructor tuple().
-
-turple1 = (12,4,5,2,6)
-
-print(turple1)
-
-print(max(turple1))
-print(len(turple1))
